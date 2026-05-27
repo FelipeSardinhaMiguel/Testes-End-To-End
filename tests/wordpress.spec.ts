2d56e2d62f46d364388c0a9e1e7c930c078890af
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-05-26 22:30:41 -0300

    testes

---
+++ tests/wordpress.spec.ts
@@ -0,0 +1,29 @@
+import {test, expect } from "@playwright/test";
+
+test.describe("E2E WordPress Playground", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://playground.wordpress.net/?language=pt_BR&mode=seamless", {
+            timeout: 60000,
+        })
+
+
+        await expect(page.getByRole("heading", { name: 'Hello world!', exact: true }),
+        ).toBeVisible({
+            timeout: 30000,
+        });
+    });
+
+    test("teste", async ({page}) => {
+        test.beforeEach(async ({ page }) => {
+        await page.goto("https://playground.wordpress.net/?language=pt_BR&mode=seamless", {
+            timeout: 60000,
+        })
+
+
+        await expect(page.getByRole("heading", { name: 'Hello world!', exact: true }),
+        ).toBeVisible({
+            timeout: 30000,
+        });
+    });
+    })
+})
